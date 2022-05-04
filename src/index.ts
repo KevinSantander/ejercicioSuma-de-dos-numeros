95663502030e51dcf12dfaf61090ae204aa941f8
Kevin <104107277+KevinSantander@users.noreply.github.com>
2022-05-04 02:54:38 -0300

    Solucion Suma de Dos Numeros

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,15 @@
 let dato1 = document.getElementById("dato1");
 let dato2 = document.getElementById("dato2");
 
-let primerNumero: number = Number(dato1.value);
-console.log("el primer número es ", primerNumero);
-let segundoNumero: number = Number(dato2.value);
-console.log("el segundo número es ", segundoNumero);
-let resultado: number = Number(primerNumero + segundoNumero);
-console.log("El resultado de la suma es ", resultado);
-
-let btnEnvdato1 = document.getElementById("btnEnviardato1");
-let btnEnvdato2 = document.getElementById("btnEnviardato2");
 let btnEnvresultado = document.getElementById("btnEnviarresultado");
-
-btnEnvdato1.addEventListener("click", () => {
-  // TU CODIGO VA AQUI, POR EJEMPLO:
-  console.log("el dato ingresado es ", dato1.value);
-  //}
-});
-
-btnEnvdato2.addEventListener("click", () => {
-  // TU CODIGO VA AQUI, POR EJEMPLO:
-  console.log("el dato ingresado es ", dato2.value);
-  //}
-});
+rotulo.innerHTML = "calculadora:";
 
 btnEnvresultado.addEventListener("click", () => {
-  // TU CODIGO VA AQUI, POR EJEMPLO:
-  console.log("el dato ingresado es ", dato1.value - dato2.value);
+  let primerNumero = Number(dato1.value);
+  console.log("el primer número es ", primerNumero);
+  let segundoNumero = Number(dato2.value);
+  console.log("el segundo número es ", segundoNumero);
+  let resultado = Number(primerNumero + segundoNumero);
+  console.log("El resultado de la suma es ", resultado);
   //}
 });
-
-// error al sumar "+" dejo mi trabajo asi como esta para luego repasar en clases sombre este problema//
